8a4378857636a9c14f3b556c5263c9b15a5bbe0a
kvik <kiram@dr.com>
2019-07-11 15:05:10 +0300

    new 2

---
+++ settings/production.py
@@ -0,0 +1,16 @@
+import os
+
+
+# Путь до коренной папки `tele_bot`
+BASE_PATH = os.path.dirname(os.path.dirname(__file__))
+
+
+TG_TOKEN = "703205633:AAHrexoyfTYHvFAgJ-BA5Zz460MX_2G2-oE"
+
+# На сервере не используем прокси-URL
+TG_API_URL = None
+
+# Валютная пара для уведомления
+NOTIFY_PAIR = "USD-BTC"
+# ID своего пользователя
+NOTIFY_USER_ID = 720951086
